8bee994e4b6ad6daee3c3e149382bfc2dbaceea7
Reza Vilera <vilera_reza99@yahoo.com>
2023-01-29 01:30:07 +0700

    initial commit

---
+++ input_items.py
@@ -0,0 +1,38 @@
+food_items = [
+        {
+        'qty':2,
+        'name':'Shrimp Ques', 
+        'price':10.00, 
+        'options':[{'qty':1, 'name': 'White Tortilla', 'price':0.00}, {'qty':2, 'name': 'Sauce', 'price':0.10}]
+        },
+        {
+        'qty':1,
+        'name':'Beef Ribs', 
+        'price':15.00, 
+        'options':[{'qty':1, 'name': 'Chili', 'price':0.00}, {'qty':2, 'name': 'Sauce', 'price':0.10}]
+        },
+        {
+        'qty':5,
+        'name':'Orange Juice', 
+        'price':3.00, 
+        'options':[{'qty':1, 'name': 'Ice', 'price':0.00}]
+        },
+        {
+        'qty':1,
+        'name':'Shrimp Ques', 
+        'price':10.00, 
+        'options':[{'qty':1, 'name': 'White Tortilla', 'price':0.00}, {'qty':1, 'name': 'Sauce', 'price':0.10}]
+        },
+        {
+        'qty':1,
+        'name':'Beef Ribs', 
+        'price':15.00, 
+        'options':[{'qty':1, 'name': 'Chili', 'price':0.00}, {'qty':1, 'name': 'Sauce', 'price':0.10}]
+        },
+        {
+        'qty':5,
+        'name':'Orange Juice', 
+        'price':3.00, 
+        'options':[{'qty':1, 'name': 'Ice', 'price':0.00}]
+        }
+        ]
